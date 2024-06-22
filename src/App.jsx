0c6580f3b325a7d8455bf126fb1a5af246edfb74
DSOrnstein <namphonggithub@gmail.com>
2024-06-22 11:17:28 +0700

    Add dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import React from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import reactLogo from "../imgs/react-icon.png";
 
-function Navbar() {
+function Navbar(props) {
   return (
-    <nav>
+    <nav className={props.darkMode ? "dark" : ""}>
       <img src={reactLogo} alt="react-icon" className="nav-icon" />
       <h3 className="nav-logo-text">ReactFacts</h3>
-      <h4 className="nav-title">React course - Project 1</h4>
+
+      <div className="toggler">
+        <p className="toggler-light">Light</p>
+        <div className="toggler-slider" onClick={props.toggleDarkMode}>
+          <div className="toggler-slider-circle"></div>
+        </div>
+        <p className="toggler-dark">Dark</p>
+      </div>
     </nav>
   );
 }
